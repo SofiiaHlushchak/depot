b8b37e9eceba691f1247a1a3ec1c484d41ce3b98
SofiiaHlushchak <glushchak02@gmail.com>
2024-10-10 18:12:33 +0200

    Add visit counter functionality and reset on adding to cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
+    update_counter
     @products = Product.order(:title)
+  end
+
+  private
+
+  def update_counter
+    session[:counter] = (session[:counter] || 0) + 1
   end
 end
