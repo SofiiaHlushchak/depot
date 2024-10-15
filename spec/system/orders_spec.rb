13b71cfba5268ff2fe388427be888082566724a1
SofiiaHlushchak <glushchak02@gmail.com>
2024-10-15 14:56:03 +0200

    Implement dynamic form fields with Stimulus and Capybara tests.

---
+++ spec/system/orders_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Orders", type: :system do
+  describe "check dynamic fields" do
+    before do
+      create(:product)
+    end
+
+    it "shows and hides fields based on payment type" do
+      visit "/"
+      click_on "Add to Cart", match: :first
+      click_on "Checkout"
+
+      expect(page).not_to have_field("Routing number")
+      expect(page).not_to have_field("Account number")
+      expect(page).not_to have_field("Credit card number")
+      expect(page).not_to have_field("Expiration date")
+      expect(page).not_to have_field("Po number")
+
+      select "Check", from: "Pay type"
+      expect(page).to have_field("Routing number")
+      expect(page).to have_field("Account number")
+      expect(page).not_to have_field("Credit card number")
+      expect(page).not_to have_field("Expiration date")
+      expect(page).not_to have_field("Po number")
+
+      select "Credit card", from: "Pay type"
+      expect(page).not_to have_field("Routing number")
+      expect(page).not_to have_field("Account number")
+      expect(page).to have_field("Credit card number")
+      expect(page).to have_field("Expiration date")
+      expect(page).not_to have_field("Po number")
+
+      select "Purchase order", from: "Pay type"
+      expect(page).not_to have_field("Routing number")
+      expect(page).not_to have_field("Account number")
+      expect(page).not_to have_field("Credit card number")
+      expect(page).not_to have_field("Expiration date")
+      expect(page).to have_field("Po number")
+    end
+  end
+end
